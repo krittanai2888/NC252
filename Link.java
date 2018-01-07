6b745408721d43da6914f4e3f1afab138d1e8940
krittanai2888 <35184954+krittanai2888@users.noreply.github.com>
2018-01-07 12:19:28 +0700

    Create Link.java

---
+++ Link.java
@@ -0,0 +1,20 @@
+package  krittanai;
+
+/**@ krittanai promsri 5907101027
+ */
+
+
+public class Link {
+	 public long data;
+	  public Link next;
+
+	  public Link(long val) {
+	    data = val;
+	    next = null;
+	  }
+
+	  public void displayLink() {
+	    System.out.print(data + " ");
+	  }
+
+	}  // end class
